5afde9951530258bbb1088f80d2473e90f4086da
manolioluigi <luigi.manolio1999@gmail.com>
2022-12-13 16:39:40 +0100

    milestone 1

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let team = [{
+    nome: "Wayne Barnett",
+    role: "Founder & CEO",
+    image: "wayne-barnett-founder-ceo.jpg"
+},
+{
+    nome: "Angela Caroll",
+    role: "Chief Editor",
+    image: "angela-caroll-chief-editor.jpg"
+},
+{
+    nome: "Walter Gordon",
+    role: "Office Manager",
+    image: "walter-gordon-office-manager.jpg"
+},
+{
+    nome: "Angela Lopez",
+    role: "Social Media Manager",
+    image: "angela-lopez-social-media-manager.jpg"
+},
+{
+    nome: "Scott Estrada",
+    role: "Developer",
+    image: "scott-estrada-developer.jpg"
+},
+{
+    nome: "Barbara Ramos",
+    role: "Graphic Designer",
+    image: "barbara-ramos-graphic-designer.jpg"
+}
+]
